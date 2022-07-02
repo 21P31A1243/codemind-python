64a01f2f51f07e2eca81e16747d4a8afa126e42e
21P31A1243 <99863252+21P31A1243@users.noreply.github.com>
2022-07-02 14:56:43 +0530

    t=int(input()) v=0 for i in range(t):     op=input()     if(op=="++X"):         v+=1     if(op=="X++"):         v+=1     if(op=="--X"):         v-=1     if(op=="X--"):         v-=1 print(v)

---
+++ Bits++.py
@@ -0,0 +1,15 @@
+t=int(input())
+v=0
+for i in range(t):
+    op=input()
+    if(op=="++X"):
+        v+=1
+    if(op=="X++"):
+        v+=1
+    if(op=="--X"):
+        v-=1
+    if(op=="X--"):
+        v-=1
+print(v)
+    
+
