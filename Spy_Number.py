63f40f0346d396b44ec55d9a57e4925bb3e31a39
21P31A1243 <99863252+21P31A1243@users.noreply.github.com>
2022-08-25 09:08:29 +0530

    n=int(input()) sum,mul=0,1 while(n!=0):     sum+=n%10     mul*=n%10     n//=10 if(sum==mul):     print("Spy Number") else:     print("Not Spy Number")

--- Spy_Number.py
+++ Spy_Number.py
@@ -1,15 +1,10 @@
-num=int(input())
-sum=0
-product=1
-num1=num
-
-while(num>0):
-    d=num%10
-    sum=sum+d
-    product=product*d
-    num=num//10
-    
-if(sum==product):
-    print('Spy Number')
+n=int(input())
+sum,mul=0,1
+while(n!=0):
+    sum+=n%10
+    mul*=n%10
+    n//=10
+if(sum==mul):
+    print("Spy Number")
 else:
-    print('Not Spy Number')
+    print("Not Spy Number")
