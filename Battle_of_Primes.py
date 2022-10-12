fedff80cbef3e8f322dce56698995d937bed81cf
21P31A1243 <99863252+21P31A1243@users.noreply.github.com>
2022-10-12 11:10:43 +0530

    def prime(a):     c=0     for i in range(1,a+1):         if a%i==0:             c+=1     if c==2:         return 1     else:         return 0 a=int(input()) b=int(input()) for i in range(1,1000):     if prime(i+a+b)==1:         print(i)         break

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,19 +1,15 @@
 def prime(a):
     c=0
-    if a!=1:
-        for i in range(2,a):
-            if(a%i)==0:
-                c=1
-                break
-        if c==0:
-            return 0
-        else:
-            return 1
-n=int(input())
-m=int(input())
-s=n+m+1
-c=1
-while prime(s):
-    s+=1
-    c+=1
-print(c)
+    for i in range(1,a+1):
+        if a%i==0:
+            c+=1
+    if c==2:
+        return 1
+    else:
+        return 0
+a=int(input())
+b=int(input())
+for i in range(1,1000):
+    if prime(i+a+b)==1:
+        print(i)
+        break
