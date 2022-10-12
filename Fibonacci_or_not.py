142781d32b8a7d09396075c58332c6035a68e1a3
21P31A1243 <99863252+21P31A1243@users.noreply.github.com>
2022-10-12 12:27:00 +0530

    def fib(n):     f=0     s=1     while(f<=1000):         if(n is f):             return 1         t=f+s         f=s         s=t n=int(input()) if(fib(n)==1):     print("True") else:     print("False")

--- Fibonacci_or_not.py
+++ Fibonacci_or_not.py
@@ -1,18 +1,14 @@
-a=int(input())
-fa=0
-fb=1
-fn=fa+fb
-c=0
-for i in range(100):
-    if fa==a:
-        c=1
-        break
-    else:
-        fn=fa+fb
-        fa=fb
-        fb=fn
-        continue
-if c==1:
+def fib(n):
+    f=0
+    s=1
+    while(f<=1000):
+        if(n is f):
+            return 1
+        t=f+s
+        f=s
+        s=t
+n=int(input())
+if(fib(n)==1):
     print("True")
 else:
     print("False")
